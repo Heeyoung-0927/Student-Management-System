37f1d0ebf7da7d063971d77d1b26dff076df87f7
Heeyoung Shin <heeyoung927@gmail.com>
2025-07-29 12:53:01 -0400

    Delete StudentRepository.java

--- StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package net.heeyoung.sms.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import net.heeyoung.sms.entity.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Long>{
-	
-}
